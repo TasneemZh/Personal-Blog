32eab9bcaefede1b52de37c7afd98189faea1ec1
TasneemZh <tasneemisfree2@gmail.com>
2021-05-01 13:13:39 +0300

    Add mongo database

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+// Node.js packages
 const express = require("express");
 var _ = require("lodash");
 const ejs = require("ejs");
 
+// Databse configuration
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/postDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Connected to the database successfully!");
+});
+
+// Constants
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -9,17 +24,31 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 const posts = [];
 
-app.set("view engine", "ejs");
+// Mongoose schema
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
 
+// Mongoose model
+const postModel = mongoose.model("posts", postSchema);
+
+// Connections
+app.set("view engine", "ejs");
 app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
 
+// Get method
 app.get("/", function(req, res) {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts
+  postModel.find(function(err, docs) {
+    if (!err) {
+      res.render("home", {
+        homeStartingContent: homeStartingContent,
+        posts: docs
+      });
+    }
   });
 });
 
@@ -39,26 +68,33 @@ app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
-app.get("/posts/:postName", function(req, res) {
-  posts.forEach(function(singlePost) {
-    if (_.lowerCase(req.params.postName) === singlePost.title.toLowerCase()) {
+app.get("/posts/:postID", function(req, res) {
+
+  postModel.findOne({
+    _id: req.params.postID
+  }, function(err, doc) {
+    if (!err) {
       res.render("post", {
-        singlePost: singlePost
+        singlePost: doc
       });
     }
   });
 });
 
+// Post method
 app.post("/compose", function(req, res) {
-  const newPost = {
+  const newPost = new postModel({
     title: req.body.postTitle,
     content: req.body.postContent
-  };
-
-  posts.push(newPost);
-  res.redirect("/");
+  });
+  newPost.save(function(err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
+// Server port
 app.listen(3000, function() {
   console.log("Server is running on port 3000...");
 });
